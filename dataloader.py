a069607abe438e982a4e76bdd7ea3a1cf7e2801e
lukashelff <lukas@helff.com>
2019-12-07 15:25:37 +0100

    save model adde

--- dataloader.py
+++ dataloader.py
@@ -22,79 +22,7 @@ from captum.attr import Saliency
 from captum.attr import visualization as viz
 from captum.attr import GuidedGradCam
 
-DEVICE = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-
-
-# returns Array of tuples(String, int) with ID and disease information 0 disease/ 1 healthy e.g. (3_Z2_1_0_1, 0)
-def load_labels():
-    path_test = 'data/test_fileids.txt'
-    path_train = 'data/train_fileids.txt'
-    valid = []
-    train = []
-    valid_s = open(path_test, 'r').readlines()
-    train_s = open(path_train, 'r').readlines()
-    for i in valid_s:
-        data = i.split(';')
-        valid.append((data[0], int(data[1])))
-    for i in train_s:
-        data = i.split(';')
-        train.append((data[0], int(data[1])))
-    return train, valid, train + valid
-
-
-# image loader
-# returns 4 Arrays labelIdS, labels, ImageIDs and the Image as a Tuple(String, mmemap) e.g. (3_Z2_1_0_1, memmap)
-def load_images_for_labels(root_path, labels, mode):
-    # loads all the images have existing entry labels
-    def load_image(path):
-        ids, ims = [], []
-        dict = pickle.load(open(path + '/data.p', 'rb'))
-        shape = dict['memmap_shape']
-        samples = dict['samples']
-        data_all = np.memmap(path + '/memmap.dat', mode='r', shape=shape, dtype='float32')
-        labels_ids = [i[0] for i in labels]
-        for k, i in enumerate(samples):
-            # only add if we have a label for the image
-            if i['id'].replace(',', '_') in labels_ids:
-                if mode == 'rgb':
-                    # ims.append(data_all[k][:, :, [50, 88, 151]].reshape(3, 255, 213))
-                    # ims.append(data_all[k][:, :, [50, 88, 151]])
-                    data = np.transpose(data_all[k][:, :, [50, 88, 151]], (2, 0, 1))
-                    ims.append(data)
-                elif mode == 'spec':
-                    # ims.append(data_all[k].reshape(3, 255, 213))
-                    ims.append(data_all[k])
-                ids.append(i['id'].replace(',', '_'))
-        return ids, ims
-
-    # removes label entries with no existing image
-    def sync_labels(im_ids):
-        labs = labels
-        for (k, i) in labels:
-            if k not in im_ids:
-                labs.remove((k, i))
-        lab_raw = [i[1] for i in labs]
-        return labs, lab_raw
-
-    loaded_images = []
-    loaded_image_ids = []
-    for i in range(1, 5):
-        print("loading images of day: " + str(i))
-        if i == 1:
-
-            for k in range(1, 14):
-                ids, im = load_image(root_path + str(i) + '_Z' + str(k) + '/segmented_leafs')
-                loaded_images += im
-                loaded_image_ids += ids
-        else:
-            for k in range(1, 19):
-                if not (k == 16 and i == 4):
-                    ids, im = load_image(root_path + str(i) + '_Z' + str(k) + '/segmented_leafs')
-                    loaded_images += im
-                    loaded_image_ids += ids
-    label_ids, label_raw = sync_labels(loaded_image_ids)
-    print("all images loaded")
-    return label_ids, label_raw, loaded_image_ids, loaded_images
+DEVICE = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 
 
 # transpose image to display learned images
@@ -144,148 +72,14 @@ def freeze_all(model_params):
         param.requires_grad = False
 
 
-class Spectralloader(Dataset):
-    """
-        The spektral dataset can be found in folder
-
-        /home/schramowski/datasets/deepplant/data/parsed_data/Z/VNIR/
-
-        The folder structure of the dataset is as follows::
-
-            └── VNIR
-                ├── 1_Z1
-                │   └── segmented_leafs
-                │       ├── data.p
-                │       └── memmap.dat
-                ├── 1_Z2
-                │   └── segmented_leafs
-                │       ├── data.p
-                │       └── memmap.dat
-                ├──
-                 ...
-                ├──
-                ├── 1_Z13
-                │   └── segmented_leafs
-                │       ├── data.p
-                │       └── memmap.dat
-                ├── 2_Z1
-                │   └── segmented_leafs
-                │       ├── data.p
-                │       └── memmap.dat
-                ├──
-                ├──
-                ├── 4_Z18
-                │   └── segmented_leafs
-                │       ├── data.p
-                │       └── memmap.dat
-
-
-                Platte Z1 - Z18
-                Tag der Aufnahme: 1
-                1_Z1 - 1_Z13
-                Tag der Aufnahme: 2
-                2_Z1 - 2_Z18
-                Tag der Aufnahme: 3
-                3_Z1 - 3_Z18
-                Tag der Aufnahme: 4
-                4_Z1 - 4_Z18
-
-
-                train and valid Dataset
-
-                Beispiel für ein Blatt:
-                1_Z3_2_0_1;0
-                    2_0_1 id des Blattes
-                    0 steht für gesund; 1 für krank
-
-                Image format
-                (213, 255, 328)
-                RGB format
-                (213, 255, 3)
-                learning format
-                (3, 255, 213)
-                RGB channels
-                [50, 88, 151]
-                SWIR
-                [24, 51, 118]
-
-        """
-
-    def __init__(self, label, labels_ids, data, data_ids, root, transform=None):
-        self.data = data
-        self.data_ids = data_ids
-        self.labels = label
-        self.labels_ids = labels_ids
-        self.transform = transform
-        self.path = root
-
-    def __getitem__(self, index):
-        # return only 1 sample and label (according to "Index")
-
-        # get label for ID
-        label = self.labels[index]
-        ID = self.labels_ids[index]
-
-        # get Image Index for Id of Label
-        index_im = [i for i, s in enumerate(self.data_ids) if ID[0] == s]
-        image = self.data[index_im[0]]
-
-        return image, label
-
-    def __len__(self):
-        return len(self.labels)
-
-
-def is_float(string):
-    try:
-        return float(string)
-    except ValueError:
-        return False
-
-
-def main():
-    mode = 'rgb'
-    # validation_split = .2
-    shuffle_dataset = True
-    random_seed = 42
-    root = '/home/schramowski/datasets/deepplant/data/parsed_data/Z/VNIR/'
-    classes = ('healthy', 'disease')
-
-    print('loading training data')
-    # load train dataset
-    train_labels, valid_labels, all_labels = load_labels()
-    labels_ids, labels_raw, image_ids, images_raw = load_images_for_labels(root, train_labels, mode=mode)
-    train_ds = Spectralloader(labels_raw, labels_ids, images_raw, image_ids, root)
-
-    print('loading validation data')
-    # load valid dataset
-    labels_ids, labels_raw, image_ids, images_raw = load_images_for_labels(root, valid_labels, mode=mode)
-    val_ds = Spectralloader(labels_raw, labels_ids, images_raw, image_ids, root)
-
-    batch_size = 20
-    n_classes = 2
-    N_EPOCHS = 50
-
+# trains and returns model for the given dataloader and computes graph acc, balanced acc and loss
+def train(batch_size, n_classes, N_EPOCHS, learning_rate, train_dl, val_dl):
     train_loss = np.zeros(N_EPOCHS)
     train_acc = np.zeros(N_EPOCHS)
     train_balanced_acc = np.zeros(N_EPOCHS)
     valid_loss = np.zeros(N_EPOCHS)
     valid_acc = np.zeros(N_EPOCHS)
     valid_balanced_acc = np.zeros(N_EPOCHS)
-
-    train_dl = DataLoader(
-        train_ds,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=4,
-    )
-
-    val_dl = DataLoader(
-        val_ds,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=4,
-    )
 
     def get_model():
         model = models.resnet18(pretrained=True)
@@ -298,7 +92,7 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(
         get_trainable(model.parameters()),
-        lr=0.0005,
+        lr=learning_rate,
         # momentum=0.9,
     )
 
@@ -370,20 +164,252 @@ def main():
             f"  balanced acc:  {valid_balanced_acc[epoch]:9.3f}%"
         )
 
+    # plot acc, balanced acc and loss
+    plt.plot(train_acc, color='skyblue', label='train acc')
+    plt.plot(valid_acc, color='orange', label='valid_acc')
+    plt.plot(train_balanced_acc, color='darkblue', label='train_balanced_acc')
+    plt.plot(valid_balanced_acc, color='red', label='valid_balanced_acc')
+    plt.ylabel('acc')
+    plt.xlabel('epoch')
+    plt.legend(loc='lower right')
+    plt.show()
+    plt.plot(train_loss, color='red', label='train_loss')
+    plt.plot(valid_loss, color='orange', label='valid_loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(loc='lower right')
+    plt.show()
+
+    return model
+
+
+# returns Array of tuples(String, int) with ID and disease information 0 disease/ 1 healthy e.g. (3_Z2_1_0_1, 0)
+def load_labels():
+    path_test = 'data/test_fileids.txt'
+    path_train = 'data/train_fileids.txt'
+    valid = []
+    train = []
+    valid_s = open(path_test, 'r').readlines()
+    train_s = open(path_train, 'r').readlines()
+    for i in valid_s:
+        data = i.split(';')
+        valid.append((data[0], int(data[1])))
+    for i in train_s:
+        data = i.split(';')
+        train.append((data[0], int(data[1])))
+    return train, valid, train + valid
+
+class Spectralloader(Dataset):
+    """
+        The spektral dataset can be found in folder
+
+        /home/schramowski/datasets/deepplant/data/parsed_data/Z/VNIR/
+
+        The folder structure of the dataset is as follows::
+
+            └── VNIR
+                ├── 1_Z1
+                │   └── segmented_leafs
+                │       ├── data.p
+                │       └── memmap.dat
+                ├── 1_Z2
+                │   └── segmented_leafs
+                │       ├── data.p
+                │       └── memmap.dat
+                ├──
+                 ...
+                ├──
+                ├── 1_Z13
+                │   └── segmented_leafs
+                │       ├── data.p
+                │       └── memmap.dat
+                ├── 2_Z1
+                │   └── segmented_leafs
+                │       ├── data.p
+                │       └── memmap.dat
+                ├──
+                ├──
+                ├── 4_Z18
+                │   └── segmented_leafs
+                │       ├── data.p
+                │       └── memmap.dat
+
+
+                Platte Z1 - Z18
+                Tag der Aufnahme: 1
+                1_Z1 - 1_Z13
+                Tag der Aufnahme: 2
+                2_Z1 - 2_Z18
+                Tag der Aufnahme: 3
+                3_Z1 - 3_Z18
+                Tag der Aufnahme: 4
+                4_Z1 - 4_Z18
+
+
+                train and valid Dataset
+
+                Beispiel für ein Blatt:
+                1_Z3_2_0_1;0
+                    2_0_1 id des Blattes
+                    0 steht für gesund; 1 für krank
+
+                Image format
+                (213, 255, 328)
+                RGB format
+                (213, 255, 3)
+                learning format
+                (3, 255, 213)
+                RGB channels
+                [50, 88, 151]
+                SWIR
+                [24, 51, 118]
+
+        """
+
+    def __init__(self, labels, root, mode, transform=None):
+        # load data for given labels
+        self.labels_ids, self.labels, self.data_ids, self.data = self.load_images_for_labels(root, labels, mode=mode)
+        self.transform = transform
+        self.path = root
+
+    def __getitem__(self, index):
+        # return only 1 sample and label (according to "Index")
+        # get label for ID
+        label = self.labels[index]
+        ID = self.labels_ids[index]
+        # get Image Index for Id of Label
+        index_im = [i for i, s in enumerate(self.data_ids) if ID[0] == s]
+        image = self.data[index_im[0]]
+
+        return image, label
+
+    def __len__(self):
+        return len(self.labels)
+
+        # image loader
+    # returns 4 Arrays labelIdS, labels, ImageIDs and the Image as a Tuple(String, mmemap) e.g. (3_Z2_1_0_1, memmap)
+    def load_images_for_labels(self, root_path, labels, mode):
+        # loads all the images have existing entry labels
+        def load_image(path):
+            ids, ims = [], []
+            dict = pickle.load(open(path + '/data.p', 'rb'))
+            shape = dict['memmap_shape']
+            samples = dict['samples']
+            data_all = np.memmap(path + '/memmap.dat', mode='r', shape=shape, dtype='float32')
+            labels_ids = [i[0] for i in labels]
+            for k, i in enumerate(samples):
+                # only add if we have a label for the image
+                if i['id'].replace(',', '_') in labels_ids:
+                    if mode == 'rgb':
+                        # ims.append(data_all[k][:, :, [50, 88, 151]].reshape(3, 255, 213))
+                        # ims.append(data_all[k][:, :, [50, 88, 151]])
+                        data = np.transpose(data_all[k][:, :, [50, 88, 151]], (2, 0, 1))
+                        ims.append(data)
+                    elif mode == 'spec':
+                        # ims.append(data_all[k].reshape(3, 255, 213))
+                        ims.append(data_all[k])
+                    ids.append(i['id'].replace(',', '_'))
+            return ids, ims
+
+        # removes label entries with no existing image
+        def sync_labels(im_ids):
+            labs = labels
+            for (k, i) in labels:
+                if k not in im_ids:
+                    labs.remove((k, i))
+            lab_raw = [i[1] for i in labs]
+            return labs, lab_raw
+
+        loaded_images = []
+        loaded_image_ids = []
+        for i in range(1, 5):
+            print("loading images of day: " + str(i))
+            if i == 1:
+
+                for k in range(1, 14):
+                    ids, im = load_image(root_path + str(i) + '_Z' + str(k) + '/segmented_leafs')
+                    loaded_images += im
+                    loaded_image_ids += ids
+            else:
+                for k in range(1, 19):
+                    if not (k == 16 and i == 4):
+                        ids, im = load_image(root_path + str(i) + '_Z' + str(k) + '/segmented_leafs')
+                        loaded_images += im
+                        loaded_image_ids += ids
+        label_ids, label_raw = sync_labels(loaded_image_ids)
+        print("all images loaded")
+        return label_ids, label_raw, loaded_image_ids, loaded_images
+
+def is_float(string):
+    try:
+        return float(string)
+    except ValueError:
+        return False
+
+
+
+def main():
+    mode = 'rgb'
+    # validation_split = .2
+    shuffle_dataset = True
+    random_seed = 42
+    root = '/home/schramowski/datasets/deepplant/data/parsed_data/Z/VNIR/'
+    classes = ('healthy', 'disease')
+
+    print('loading training data')
+    # load train dataset
+    train_labels, valid_labels, all_labels = load_labels()
+    train_ds = Spectralloader(train_labels, root, mode)
+
+    print('loading validation data')
+    # load valid dataset
+    val_ds = Spectralloader(valid_labels, root, mode)
+
+    batch_size = 20
+    n_classes = 2
+    N_EPOCHS = 50
+    lr = 0.00025
+    retrain = False
+    filename = 'data/trained_model.sav'
+
+    # dataloader
+    train_dl = DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=4,
+    )
+
+    val_dl = DataLoader(
+        val_ds,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=4,
+    )
+
+    # train model or use trained model from last execution
+    if retrain:
+        model = train(batch_size, n_classes, N_EPOCHS, lr, train_dl, val_dl)
+        pickle.dump(model, open(filename, 'wb'))
+    else:
+        model = pickle.load(open(filename, 'rb'))
+
+    # get first batch for evaluation
     dataiter = iter(val_dl)
     image1, label1 = next(dataiter)
 
-    # print images
+    # print images of first batch
     display_rgb_grid(torchvision.utils.make_grid(image1), 'loader')
 
-    print('GroundTruth: ', ' '.join('%5s' % classes[label1[j]] for j in range(8)))
+    print('GroundTruth: ', ' '.join('%5s' % classes[label1[j]] for j in range(batch_size)))
 
     outputs = model(image1.to(DEVICE))
 
     _, predicted = torch.max(outputs, 1)
 
-    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(8)))
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(batch_size)))
 
+    #
     ind = 1
     input = image1[ind].unsqueeze(0)
     input.requires_grad = True
@@ -433,33 +459,17 @@ def main():
 
     original_image = np.transpose(image1[ind].cpu().detach().numpy(), (1, 2, 0))
 
-    # plot acc train and train loss
-    plt.plot(train_acc, color='skyblue', label='train acc')
-    plt.plot(valid_acc, color='orange', label='valid_acc')
-    plt.plot(train_balanced_acc, color='darkblue', label='train_balanced_acc')
-    plt.plot(valid_balanced_acc, color='red', label='valid_balanced_acc')
-    plt.ylabel('acc')
-    plt.xlabel('epoch')
-    plt.legend(loc='lower right')
-    plt.show()
-    plt.plot(train_loss, color='red', label='train_loss')
-    plt.plot(valid_loss, color='orange', label='valid_loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(loc='lower right')
-    plt.show()
-
     _ = viz.visualize_image_attr(None, original_image,
                                  method="original_image", title="Original Image")
 
-    _ = viz.visualize_image_attr(grads, original_image, method="blended_heat_map", sign="absolute_value",
+    _ = viz.visualize_image_attr(grads, original_image, sign="absolute_value", method="blended_heat_map",
                                  show_colorbar=True, title="Overlayed Gradient Magnitudes saliency")
 
-    _ = viz.visualize_image_attr(attr_ig, original_image, method="blended_heat_map", sign="all",
+    _ = viz.visualize_image_attr(attr_ig, original_image, sign="all", method="blended_heat_map",
                                  show_colorbar=True, title="Overlayed Integrated Gradients")
     #
-    _ = viz.visualize_image_attr(attr_ig_nt, original_image, method="blended_heat_map", sign="absolute_value",
-                                 outlier_perc=10, show_colorbar=True,
+    _ = viz.visualize_image_attr(attr_ig_nt, original_image, sign="absolute_value",
+                                 outlier_perc=10, show_colorbar=True, method="blended_heat_map",
                                  title="Overlayed Noise Tunnel \n with SmoothGrad Squared")
 
     # default_cmap = LinearSegmentedColormap.from_list('custom blue',
@@ -474,7 +484,7 @@ def main():
     #                                       cmap=default_cmap,
     #                                       show_colorbar=True)
 
-    _ = viz.visualize_image_attr(attr_gc, original_image, method="blended_heat_map", sign="absolute_value",
+    _ = viz.visualize_image_attr(attr_gc, original_image, sign="absolute_value", method="blended_heat_map",
                                  show_colorbar=True, title="Overlayed GuidedGradCam")
 
     # _ = viz.visualize_image_attr(attr_dl, original_image, method="blended_heat_map",sign="all",show_colorbar=True,
