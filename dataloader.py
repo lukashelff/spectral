90cbd9f366751b345a11f0589547ed314c123a96
lukashelff <lukas@helff.com>
2019-12-10 05:39:48 +0100

    compare

--- dataloader.py
+++ dataloader.py
@@ -7,6 +7,9 @@ from PIL import Image as PImage
 from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets.folder import ImageFolder, default_loader
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import numpy as np
 import torch
 import torch.nn as nn
@@ -21,8 +24,9 @@ from captum.attr import IntegratedGradients, NoiseTunnel, DeepLift
 from captum.attr import Saliency
 from captum.attr import visualization as viz
 from captum.attr import GuidedGradCam
+from captum.attr._core.guided_grad_cam import LayerGradCam
 
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 
 
 # transpose image to display learned images
@@ -172,15 +176,174 @@ def train(batch_size, n_classes, N_EPOCHS, learning_rate, train_dl, val_dl):
     plt.ylabel('acc')
     plt.xlabel('epoch')
     plt.legend(loc='lower right')
+    plt.savefig('./data/plots/accuracy.png')
     plt.show()
     plt.plot(train_loss, color='red', label='train_loss')
     plt.plot(valid_loss, color='orange', label='valid_loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(loc='lower right')
+    plt.savefig('./data/plots/loss.png')
     plt.show()
 
     return model
+
+
+def explain(model, image, label):
+    input = image.unsqueeze(0)
+    input.requires_grad = True
+    model.eval()
+
+    def attribute_image_features(algorithm, input, **kwargs):
+        model.zero_grad()
+        tensor_attributions = algorithm.attribute(input,
+                                                  target=label,
+                                                  **kwargs
+                                                  )
+
+        return tensor_attributions
+
+    # saliency
+    saliency = Saliency(model)
+    grads = saliency.attribute(input, target=label)
+    grads = np.transpose(grads.squeeze().cpu().detach().numpy(), (1, 2, 0))
+
+    # IntegratedGradients
+    ig = IntegratedGradients(model)
+    attr_ig, delta = attribute_image_features(ig, input, baselines=input * 0, return_convergence_delta=True)
+    attr_ig = np.transpose(attr_ig.squeeze().cpu().detach().numpy(), (1, 2, 0))
+
+    # IntegratedGradients Noise Tunnel
+    ig = IntegratedGradients(model)
+    nt = NoiseTunnel(ig)
+    attr_ig_nt = attribute_image_features(nt, input, baselines=input * 0, nt_type='smoothgrad_sq',
+                                          # n_samples=100,
+                                          stdevs=0.2)
+    attr_ig_nt = np.transpose(attr_ig_nt.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+    # GuidedGradCam
+    gc = GuidedGradCam(model, model.layer4)
+    attr_gc = attribute_image_features(gc, input)
+    attr_gc = np.transpose(attr_gc.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+    # GradCam
+    gco = LayerGradCam(model, model.layer4)
+    attr_gco = attribute_image_features(gco, input)
+    attr_gco = np.transpose(attr_gco.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+    # # Deeplift
+    # dl = DeepLift(model)
+    # attr_dl = attribute_image_features(dl, input)
+    # attr_dl = np.transpose(attr_dl.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
+
+    # print('Leaf is ', classes[predicted[ind]],
+    #       'with a Probability of:', torch.max(F.softmax(outputs, 1)).item())
+
+    original_image = np.transpose(image.cpu().detach().numpy(), (1, 2, 0))
+    # Original Image
+    f1, a1 = viz.visualize_image_attr(None, original_image,
+                                      method="original_image",
+                                      # title="Original Image",
+                                      use_pyplot=False)
+    # Overlayed Gradient Magnitudes saliency
+    f2, a2 = viz.visualize_image_attr(grads, original_image, sign="absolute_value", method="blended_heat_map",
+                                      # show_colorbar=True, title="Overlayed Gradient Magnitudes saliency",
+                                      use_pyplot=False)
+    # Overlayed Integrated Gradients
+    f3, a3 = viz.visualize_image_attr(attr_ig, original_image, sign="all", method="blended_heat_map",
+                                      # show_colorbar=True, title="Overlayed Integrated Gradients",
+                                      use_pyplot=False)
+    # Overlayed Noise Tunnel
+    f4, a4 = viz.visualize_image_attr(attr_ig_nt, original_image, sign="absolute_value",
+                                      outlier_perc=10, method="blended_heat_map", use_pyplot=False,
+                                      # title="Overlayed Noise Tunnel \n with SmoothGrad Squared", show_colorbar=True
+                                      )
+    # # DeepLift
+    # f5, a5 = viz.visualize_image_attr(attr_dl, original_image, method="blended_heat_map", sign="all",
+    #                                   # show_colorbar=True, title="Overlayed DeepLift"
+    #                                   )
+
+    # GuidedGradCam
+    f6, a6 = viz.visualize_image_attr(attr_gc, original_image, sign="absolute_value", method="blended_heat_map",
+                                      show_colorbar=False, use_pyplot=False)
+
+    # GradCam
+    f7, a7 = viz.visualize_image_attr(attr_gco, original_image, sign="absolute_value", method="blended_heat_map",
+                                      show_colorbar=False, use_pyplot=False)
+
+    return [f1, f2, f3, f4, f6, f7]
+
+
+def plotexplainer(pathroot, explainers, imageclass, numdis):
+    # compare images between classes
+    classnum = len(imageclass)
+    exnum = len(explainers)
+    print('Ä')
+    print(classnum)
+    print(exnum)
+    fig = plt.figure(figsize=(6 * exnum, 6 * classnum))
+    # fig.set_title('Class comparison TP, FP, TN, FN plant images')
+    print("hallo")
+    for k in range(0, classnum):
+        for i in range(0, exnum):
+            img = mpimg.imread(pathroot + explainers[i] + imageclass[k] + '.png')
+            ax = fig.add_subplot(classnum, exnum, (i + 1) + k * exnum)
+            plt.imshow(img)
+            ax.tick_params(axis='both', which='both', length=0)
+            plt.setp(ax.get_xticklabels(), visible=False)
+            plt.setp(ax.get_yticklabels(), visible=False)
+            if k == 0:
+                ax.set_title(explainers[i], fontsize=25)
+            if i == 0:
+                if imageclass[k] == 'tp':
+                    ax.set_ylabel('TP:\n Truth: healthy\n Prediction: healthy', fontsize=25)
+                if imageclass[k] == 'fp':
+                    ax.set_ylabel('FP:\n Truth: diseased\n Prediction: healthy', fontsize=25)
+                if imageclass[k] == 'tn':
+                    ax.set_ylabel('TN:\n Truth: diseased\n Prediction: diseased', fontsize=25)
+                if imageclass[k] == 'fn':
+                    ax.set_ylabel('FN:\n Truth: healthy\n Prediction: diseased', fontsize=25)
+    fig.tight_layout()
+    fig.savefig(pathroot + 'conclusion' + '.png')
+    plt.show()
+
+    # compare detected diseased images of plantes
+    fig = plt.figure(figsize=(6 * exnum, 6 * numdis))
+    # fig.set_title('comparison between detected diseases')
+    for k in range(0, numdis):
+        for i in range(0, exnum):
+            img = mpimg.imread(pathroot + 'diseasedims/' + explainers[i] + 'diseased' + str(k) + '.png')
+            ax = fig.add_subplot(numdis, exnum, (i + 1) + k * exnum)
+            plt.imshow(img)
+            ax.tick_params(axis='both', which='both', length=0)
+            plt.setp(ax.get_xticklabels(), visible=False)
+            plt.setp(ax.get_yticklabels(), visible=False)
+            if k == 0:
+                ax.set_title(explainers[i], fontsize=25)
+            if i == 0:
+                ax.set_ylabel('diseased image ' + str(k), fontsize=25)
+    fig.tight_layout()
+    fig.savefig(pathroot + 'diseasedims/' + 'conclusion' + '.png')
+    plt.show()
+
+    # compare detected diseased images of plantes
+    fig = plt.figure(figsize=(6 * exnum, 6 * numdis))
+    # fig.set_title('comparison between not detected diseases')
+    for k in range(0, numdis):
+        for i in range(0, exnum):
+            img = mpimg.imread(pathroot + 'diseasedimsnotdetected/' + explainers[i] + 'diseased' + str(k) + '.png')
+            ax = fig.add_subplot(numdis, exnum, (i + 1) + k * exnum)
+            plt.imshow(img)
+            ax.tick_params(axis='both', which='both', length=0)
+            plt.setp(ax.get_xticklabels(), visible=False)
+            plt.setp(ax.get_yticklabels(), visible=False)
+            if k == 0:
+                ax.set_title(explainers[i], fontsize=25)
+            if i == 0:
+                ax.set_ylabel('diseased image ' + str(k), fontsize=25)
+    fig.tight_layout()
+    fig.savefig(pathroot + 'notdetecteddiseasedims/' + 'conclusion' + '.png')
+    plt.show()
 
 
 # returns Array of tuples(String, int) with ID and disease information 0 disease/ 1 healthy e.g. (3_Z2_1_0_1, 0)
@@ -350,12 +513,23 @@ def is_float(string):
         return False
 
 
+# create canvas man to show saved figures
+def show_figure(fig, ax):
+    # create a dummy figure and use its
+    # manager to display the original figure
+    dummy = plt.figure()
+    new_manager = dummy.canvas.manager
+    new_manager.canvas.figure = fig
+    # new_manager.canvas.axes = ax
+    fig.set_canvas(new_manager.canvas)
+
+
 def main():
     mode = 'rgb'
     shuffle_dataset = True
     random_seed = 42
     root = '/home/schramowski/datasets/deepplant/data/parsed_data/Z/VNIR/'
-    classes = ('healthy', 'disease')
+    classes = ('healthy', 'diseased')
 
     print('loading training data')
     # load train dataset
@@ -371,6 +545,7 @@ def main():
     N_EPOCHS = 50
     lr = 0.00025
     retrain = False
+    reexplain = False
     filename = 'data/trained_model.sav'
 
     # dataloader
@@ -395,119 +570,97 @@ def main():
     else:
         model = pickle.load(open(filename, 'rb'))
 
-    # get first batch for evaluation
-    dataiter = iter(val_dl)
-    image1, label1 = next(dataiter)
-
-    # print images of first batch
-    display_rgb_grid(torchvision.utils.make_grid(image1), 'loader')
-
-    print('GroundTruth: ', ' '.join('%5s' % classes[label1[j]] for j in range(batch_size)))
-
-    outputs = model(image1.to(DEVICE))
-
-    _, predicted = torch.max(outputs, 1)
-
-    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(batch_size)))
-
-    #
-    ind = 1
-    input = image1[ind].unsqueeze(0)
-    input.requires_grad = True
+    # get index for each class
+    # actual: healthy prediction: healthy, true positive, image 8 of sixth batch
+    # actual: diseased prediction: healthy, false positive, image 12 of sixth batch
+    # actual: diseased prediction: diseased, true negative, image 0 of first batch
+    # actual: healthy prediction: diseased, false negative, image 7 of first batch
+    indclasses = [-1, -1, -1, -1]
+    indclassesIM = [0, 0, 0, 0]
+    # 6 images of plants with detected diseased
+    inddetdis = []
+    inddetdisIM = []
+    # 6 images of plants with not detected diseased
+    indnotdetdis = []
+    indnotdetdisIM = []
+    # Predict val dataset with final trained resnet, 1 = disease, 0 = no disease
     model.eval()
+    pred, labels = [], []
+    with torch.no_grad():
+        for X, y in val_dl:
+            X, y = X.to(DEVICE), y.to(DEVICE)
+            y_ = model(X)
+            _, y_label_ = torch.max(y_, 1)
+            pred += y_label_.tolist()
+            labels += y.tolist()
+            Xdata, ydata, preddata = X, y.tolist(), y_label_.tolist()
+            # get images and labels for images which get displayed
+            for i in range(0, len(y)):
+                if len(inddetdis) < 6 and preddata[i] == 1 and ydata[i] == 1:
+                    inddetdis += [ydata[i]]
+                    inddetdisIM.append(explain(model, Xdata[i], ydata[i]))
+                if len(indnotdetdis) < 6 and preddata[i] == 0 and y[i] == 1:
+                    indnotdetdis += [ydata[i]]
+                    indnotdetdisIM.append(explain(model, Xdata[i], ydata[i]))
+                if indclasses[2] == -1 and preddata[i] == 1 and ydata[i] == 1:
+                    indclasses[2] = ydata[i]
+                    indclassesIM[2] = explain(model, Xdata[i], ydata[i])
+                if indclasses[3] == -1 and preddata[i] == 0 and ydata[i] == 1:
+                    indclasses[3] = ydata[i]
+                    indclassesIM[3] = explain(model, Xdata[i], ydata[i])
+                if indclasses[0] == -1 and preddata[i] == 0 and ydata[i] == 0:
+                    indclasses[0] = ydata[i]
+                    indclassesIM[0] = explain(model, Xdata[i], ydata[i])
+                if indclasses[1] == -1 and preddata[i] == 1 and ydata[i] == 0:
+                    indclasses[1] = ydata[i]
+                    indclassesIM[1] = explain(model, Xdata[i], ydata[i])
 
-    def attribute_image_features(algorithm, input, **kwargs):
-        model.zero_grad()
-        tensor_attributions = algorithm.attribute(input,
-                                                  target=label1[ind],
-                                                  **kwargs
-                                                  )
-
-        return tensor_attributions
-
-    # saliency
-    saliency = Saliency(model.to("cpu"))
-    grads = saliency.attribute(input, target=label1[ind].item())
-    grads = np.transpose(grads.squeeze().cpu().detach().numpy(), (1, 2, 0))
-
-    # IntegratedGradients
-    ig = IntegratedGradients(model)
-    attr_ig, delta = attribute_image_features(ig, input, baselines=input * 0, return_convergence_delta=True)
-    attr_ig = np.transpose(attr_ig.squeeze().cpu().detach().numpy(), (1, 2, 0))
-
-    # IntegratedGradients Noise Tunnel
-    ig = IntegratedGradients(model)
-    nt = NoiseTunnel(ig)
-    attr_ig_nt = attribute_image_features(nt, input, baselines=input * 0, nt_type='smoothgrad_sq',
-                                          # n_samples=100,
-                                          stdevs=0.2)
-    attr_ig_nt = np.transpose(attr_ig_nt.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-
-    # GuidedGradCam
-    gc = GuidedGradCam(model, model.layer4)
-    attr_gc = attribute_image_features(gc, input)
-    attr_gc = np.transpose(attr_gc.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-
-    # # Deeplift
-    # dl = DeepLift(model)
-    # attr_dl = attribute_image_features(dl, input, baselines=input * 0)
-    # attr_dl = np.transpose(attr_dl.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-
-    print('Predicted:', classes[predicted[ind]],
-          ' Probability:', torch.max(F.softmax(outputs, 1)).item())
-
-    original_image = np.transpose(image1[ind].cpu().detach().numpy(), (1, 2, 0))
-
-    _ = viz.visualize_image_attr(None, original_image,
-                                 method="original_image", title="Original Image", use_pyplot=False)
-
-    _ = viz.visualize_image_attr(grads, original_image, sign="absolute_value", method="blended_heat_map",
-                                 show_colorbar=True, title="Overlayed Gradient Magnitudes saliency", use_pyplot=False)
-
-    _ = viz.visualize_image_attr(attr_ig, original_image, sign="all", method="blended_heat_map",
-                                 show_colorbar=True, title="Overlayed Integrated Gradients", use_pyplot=False)
+    # # get first batch for evaluation
+    # dataiter = iter(val_dl)
+    # image1, label1 = next(dataiter)
     #
-    f2, a2 = viz.visualize_image_attr(attr_ig_nt, original_image, sign="absolute_value",
-                                 outlier_perc=10, show_colorbar=True, method="blended_heat_map",
-                                 title="Overlayed Noise Tunnel \n with SmoothGrad Squared", use_pyplot=False)
-
-    # default_cmap = LinearSegmentedColormap.from_list('custom blue',
-    #                                                  [(0, '#ffffff'),
-    #                                                   (0.25, '#000000'),
-    #                                                   (1, '#000000')], N=256)
+    # # print images of first batch
+    # display_rgb_grid(torchvision.utils.make_grid(image1), 'images of batch 1')
+    # # predict images of first batch
+    # print('GroundTruth: ', ' '.join('%5s' % classes[label1[j]] for j in range(batch_size)))
     #
-    # _ = viz.visualize_image_attr_multiple((attr_ig_nt.squeeze()),
-    #                                       np.transpose(input.squeeze().cpu().detach().numpy(), (1,2,0)),
-    #                                       ["original_image", "heat_map"],
-    #                                       ["all", "positive"],
-    #                                       cmap=default_cmap,
-    #                                       show_colorbar=True)
+    # outputs1 = model(image1.to(DEVICE))
+    #
+    # _, predicted1 = torch.max(outputs1, 1)
+    #
+    # print('Predicted: ', ' '.join('%5s' % classes[predicted1[j]] for j in range(batch_size)))
 
-    f1, a1 = viz.visualize_image_attr(attr_gc, original_image, sign="absolute_value", method="blended_heat_map",
-                                      show_colorbar=True, title="Overlayed GuidedGradCam", use_pyplot=False)
+    # save the explainer images of the figures
+    pathroot = './data/exp/'
+    imageclass = []
+    explainers = ['Original', 'saliency', 'IntegratedGradients', 'NoiseTunnel', 'GuidedGradCam', 'GradCam']
+    numdis = min(len(inddetdis), len(indnotdetdis))
 
+    # create explainer Image and save it in files
+    if reexplain:
 
-    def show_figure(fig, ax):
+        # save images of explainer in data
+        # save num diseased images which got detected
+        for k in range(0, len(inddetdis)):
+            for i in range(0, len(explainers)):
+                inddetdisIM[k][i].savefig(pathroot + 'diseasedims/' + explainers[i] + 'diseased' + str(k) + '.png',
+                                          bbox_inches='tight')
 
-        # create a dummy figure and use its
-        # manager to display the original figure
-        dummy = plt.figure()
-        new_manager = dummy.canvas.manager
-        new_manager.canvas.figure = fig
-        # new_manager.canvas.axes = ax
-        fig.set_canvas(new_manager.canvas)
-        fig.add_axes(ax)
+        # save images of explainer in data
+        # save num diseased images which got not detected
+        for k in range(0, len(inddetdis)):
+            for i in range(0, len(explainers)):
+                indnotdetdisIM[k][i].savefig(
+                    pathroot + 'diseasedimsnotdetected/' + explainers[i] + 'diseased' + str(k) + '.png',
+                    bbox_inches='tight')
 
-    show_figure(f1, a1)
-    f1.show()
+        # save images of every class to compare
+        for k in range(0, len(imageclass)):
+            for i in range(0, len(explainers)):
+                indclassesIM[k][i].savefig(pathroot + explainers[i] + imageclass[k] + '.png', bbox_inches='tight')
 
-    # fig, ax = plt.subplots(111)
-    # ax = a1
-
-    # fig3, (ax3, ax4) = plt.subplots(1, 2, sharey=True, sharex=True, figsize=(10, 5))
-
-    # _ = viz.visualize_image_attr(attr_dl, original_image, method="blended_heat_map",sign="all",show_colorbar=True,
-    #                           title="Overlayed DeepLift")
+    # plot created explainer
+    plotexplainer(pathroot, explainers, imageclass, numdis)
 
 
 main()
