numerical fix

explainer.py
@@ -180,7 +180,7 @@ def explain_single(model, image, label, explainer):
     heapmap = np.random.rand(h, w)
 
     def cut_and_shape(data):
-        # consider only the positive values
+        # # consider only the positive values
         for i in range(h):
             for k in range(w):
                 for j in range(c):

main.py
@@ -33,14 +33,14 @@ from explainer import *
 from plots import *
 from helpfunctions import *
 
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 retrain = False
 plot_for_image_id, plot_classes, plot_categories = False, False, False
 roar_create_mask = False
-roar_train = True
+roar_train = False
 plot_roar_curve = False
-roar_mod_im_comp = False
-roar_expl_im = False
+roar_mod_im_comp = True
+roar_expl_im = True
 N_EPOCHS = 120
 lr = 0.00015
 
@@ -65,10 +65,12 @@ def load_labels():
 
 
 def main():
-    roar_explainers = ['guided_gradcam', 'noisetunnel', 'random', 'gradcam', 'noisetunnel_gaussian',
-                       'guided_gradcam_gaussian']
+    roar_explainers = ['guided_gradcam', 'random', 'gradcam', 'noisetunnel_gaussian',
+                       'guided_gradcam_gaussian', 'noisetunnel']
+    # roar_explainers = ['guided_gradcam']
     # roar_explainers = ['noisetunnel', 'random', 'gradcam', 'guided_gradcam']
     roar_values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 100]
+    # roar_values = [100]
     mode = 'rgb'
     shuffle_dataset = True
     random_seed = 42
@@ -106,13 +108,11 @@ def main():
         if not os.path.exists('./data/models/'):
             os.makedirs('./data/models/')
         torch.save(original_model.state_dict(), original_trained_model)
-        # pickle.dump(model, open(original_trained_model, 'wb'))
         print('trained model saved')
     if plot_categories or plot_classes or plot_for_image_id or roar_create_mask:
         original_model = models.resnet18(pretrained=True)
         original_model.fc = nn.Linear(512, n_classes)
         original_model.load_state_dict(torch.load(original_trained_model, map_location=DEVICE))
-        # model = pickle.load(open(original_trained_model, 'rb'))
 
     # save the created explainer Image
     if plot_classes or plot_categories:
@@ -142,10 +142,12 @@ def main():
 
     # comparison of modified roar Images
     if roar_mod_im_comp:
+        print('creating ROAR comparison plot')
         eval_roar_mod_im_comp(mode)
 
     # interpretation/explaination of modified roar Images
     if roar_expl_im:
+        print('creating ROAR explanation plot')
         eval_roar_expl_im(mode, DEVICE)
 
 

roar.py
@@ -83,7 +83,6 @@ def eval_roar_expl_im(mode, DEVICE):
                     # loading model of explainer for corresponding remove value
                     all_ds = Spectralloader([image_labels[k]], root, mode)
                     if i == 0:
-                        # model = pickle.load(open(original_trained_model, 'rb'))
                         model = models.resnet18(pretrained=True)
                         model.fc = nn.Linear(512, n_classes)
                         model.load_state_dict(torch.load(original_trained_model, map_location=DEVICE))
@@ -92,7 +91,6 @@ def eval_roar_expl_im(mode, DEVICE):
                         model.fc = nn.Linear(512, n_classes)
                         model.load_state_dict(
                             torch.load(trained_roar_models + '_' + ex + '_' + str(i) + '.pt', map_location=DEVICE))
-                        # model = pickle.load(open(trained_roar_models + '_' + ex + '_' + str(i) + '.sav', 'rb'))
                         all_ds.apply_roar_single_image(i, mask, id, 'mean', ex)
                     # plot_explained_images(model, all_ds, DEVICE, explainers, image_ids, str(i) + "%removed")
                     image, label = all_ds.get_by_id(id)
@@ -100,7 +98,7 @@ def eval_roar_expl_im(mode, DEVICE):
                     image = torch.from_numpy(image).to(DEVICE)
                     activation_map = explain_single(model, image, label, ex)
                     org = np.transpose(image.squeeze().cpu().detach().numpy(), (1, 2, 0))
-                    org_img_edged = preprocessing.scale(np.array(org, dtype=float)[:, :, 1] / 255)
+                    org_img_edged = preprocessing.scale(np.array(org, dtype=float)[:, :, 1])
                     org_img_edged = ndi.gaussian_filter(org_img_edged, 4)
                     # Compute the Canny filter for two values of sigma
                     org_img_edged = feature.canny(org_img_edged, sigma=3)
@@ -115,6 +113,7 @@ def eval_roar_expl_im(mode, DEVICE):
                     plt.setp(ax.get_yticklabels(), visible=False)
 
         fig.savefig(path_exp + subpath + 'comparison_explained_roar_image_' + id + '.png')
+        plt.show()
         plt.close('all')
 
 
@@ -163,4 +162,5 @@ def eval_roar_mod_im_comp(mode):
                     plt.setp(ax.get_xticklabels(), visible=False)
                     plt.setp(ax.get_yticklabels(), visible=False)
         fig.savefig(path_exp + subpath + 'comparison_roar_images' + id + '.png')
+        plt.show()
         plt.close('all')

spectralloader.py
@@ -178,39 +178,6 @@ class Spectralloader(Dataset):
         label_ids, label_raw = sync_labels(loaded_image_ids)
         return label_ids, label_raw, loaded_image_ids, loaded_images
 
-    # apply the roar to the dataset
-    # given percentage of the values get removed from the dataset
-    def apply_roar(self, percentage, masks, DEVICE, explainer):
-        length = self.__len__()
-        print('modified images: 0%')
-        for d in range(0, length):
-            if ((d + 1) % (length // 4)) == 0:
-                print('modified images: ' + str(round((d + 1) * 100 / self.__len__())) + '%')
-            im, label = self.__getitem__(d)
-            id = self.get_id_by_index(d)
-            mask = masks[id]
-            torch.from_numpy(im).to(DEVICE)
-            torch.from_numpy(mask).to(DEVICE)
-            mean = np.mean(im)
-            try:
-                # only take percentile of values with duplicated zeros deleted
-                mask_flat = mask.flatten()
-                if explainer == 'guided_gradcam':
-                    mask_flat = mask_flat[mask_flat != 0]
-                    np.append(mask_flat, 0)
-                percentile = np.percentile(mask_flat, 100 - percentage)
-                c, h, w = im.shape
-                val = im
-                for i in range(0, w):
-                    for j in range(0, h):
-                        if mask[j][i] >= percentile:
-                            val[0][j][i] = mean
-                            val[1][j][i] = mean
-                            val[2][j][i] = mean
-                self.update_data(id, val)
-            except ValueError:
-                print('No roar img for id: ' + id)
-
     def apply_roar_single_image(self, percentage, masks, id, new_val, explainer):
         try:
             im, label = self.get_by_id(id)
@@ -219,9 +186,9 @@ class Spectralloader(Dataset):
                 mask = masks[id]
                 # only take percentile of values with duplicated zeros deleted
                 mask_flat = mask.flatten()
-                if explainer == 'guided_gradcam':
-                    mask_flat = mask_flat[mask_flat != 0]
-                    np.append(mask_flat, 0)
+                # if explainer == 'guided_gradcam':
+                #     mask_flat = mask_flat[mask_flat != 0]
+                #     np.append(mask_flat, 0)
                 percentile = np.percentile(mask_flat, 100 - percentage)
                 c, h, w = im.shape
                 val = im
@@ -239,3 +206,39 @@ class Spectralloader(Dataset):
                 self.update_data(id, val)
         except ValueError:
             print('No roar img for id: ' + id)
+
+    # apply the roar to the dataset
+    # given percentage of the values get removed from the dataset
+    def apply_roar(self, percentage, masks, DEVICE, explainer):
+        length = self.__len__()
+        print('modified images: 0%')
+        for d in range(0, length):
+            if ((d + 1) % (length // 4)) == 0:
+                print('modified images: ' + str(round((d + 1) * 100 / self.__len__())) + '%')
+            # im, label = self.__getitem__(d)
+            id = self.get_id_by_index(d)
+            self.apply_roar_single_image(percentage, masks, id, "mean", explainer)
+
+            # mask = masks[id]
+            # torch.from_numpy(im).to(DEVICE)
+            # torch.from_numpy(mask).to(DEVICE)
+            # mean = np.mean(im)
+            # try:
+            #     # only take percentile of values with duplicated zeros deleted
+            #     mask_flat = mask.flatten()
+            #     if explainer == 'guided_gradcam':
+            #         mask_flat = mask_flat[mask_flat != 0]
+            #         np.append(mask_flat, 0)
+            #     percentile = np.percentile(mask_flat, 100 - percentage)
+            #     c, h, w = im.shape
+            #     val = im
+            #     for i in range(0, w):
+            #         for j in range(0, h):
+            #             if mask[j][i] >= percentile:
+            #                 val[0][j][i] = mean
+            #                 val[1][j][i] = mean
+            #                 val[2][j][i] = mean
+            #     self.update_data(id, val)
+            # except ValueError:
+            #     print('No roar img for id: ' + id)
+