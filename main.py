3cb44f18f0a1391ba71a1f4fe79f56c44b926473
lukashelff <lukas@helff.com>
2020-03-22 15:12:04 +0100

    captum lrp added

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ from helpfunctions import *
 DEVICE = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 retrain = False
 plot_for_image_id, plot_classes, plot_categories = False, False, False
-roar_create_mask = False
+roar_create_mask = True
 roar_train = True
 plot_roar_curve = True
 roar_mod_im_comp = False
@@ -70,9 +70,8 @@ def load_labels():
 def main():
     roar_explainers = ['gradcam', 'guided_gradcam', 'guided_gradcam_gaussian',
                        'noisetunnel', 'noisetunnel_gaussian', 'Integrated_Gradients']
-    roar_explainers = ['noisetunnel']
+    roar_explainers = ['lrp']
     roar_values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 100]
-    roar_values = [99]
     mode = 'rgb'
     shuffle_dataset = True
     random_seed = 42
